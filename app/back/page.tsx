169584f60e9667806bdc0446f182f703ddb1f30c
Gabriel <gabriel.bachelor@gmail.com>
2025-01-10 15:53:55 +0000

    x

--- app/back/page.tsx
+++ app/back/page.tsx
@@ -7,23 +7,23 @@ type Greeting = {
   greet: string;
 };
 export default async function Back() {
-    return <div></div>
-//   const baseUrl = "https://backe-nd.vercel.app/";
-//   const response = await fetch(`${baseUrl}/api`);
-//   const greetings: Greeting[] = await response.json();
 
-//   return (
-//     <div className="  flex flex-col px-2 gap-1">
-//       {greetings.map((greetingObj) => (
-//         <EditGreeting
-//           greetingObj={greetingObj}
-//           key={greetingObj._id.toString()}
-//         />
-//       ))}
-//     </div>
-//   );
-// }
+  const baseUrl = "https://backe-nd.vercel.app";
+  const response = await fetch(`${baseUrl}/api`);
+  const greetings: Greeting[] = await response.json();
+
+  return (
+    <div className="  flex flex-col px-2 gap-1">
+      {greetings.map((greetingObj) => (
+        <EditGreeting
+          greetingObj={greetingObj}
+          key={greetingObj._id.toString()}
+        />
+      ))}
+    </div>
+  );
 }
+
 // <div>
 // {greetings.map((greetingObj) => <Delete greetingObj={greetingObj}  key={greetingObj._id.toString()}/>)}
 // </div>
