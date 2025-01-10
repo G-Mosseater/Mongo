627b95c998b37afc0354a14c485984cb28e0ebf4
Gabriel <gabriel.bachelor@gmail.com>
2025-01-10 15:51:45 +0000

    x

--- app/back/page.tsx
+++ app/back/page.tsx
@@ -1,30 +1,29 @@
-
-'use server'
-import EditGreeting from "@/app/components/editGreeting"
+"use server";
+import EditGreeting from "@/app/components/editGreeting";
 
 type Greeting = {
-    greeting: string;
-    _id: string;
-    greet: string
-}
+  greeting: string;
+  _id: string;
+  greet: string;
+};
 export default async function Back() {
-    const baseUrl = "http://localhost:3000"
-    const response = await fetch (`${baseUrl}/api`)
-    const greetings: Greeting[] = await response.json()
+    return <div></div>
+//   const baseUrl = "https://backe-nd.vercel.app/";
+//   const response = await fetch(`${baseUrl}/api`);
+//   const greetings: Greeting[] = await response.json();
 
-return (
-
-        <div className="  flex flex-col px-2 gap-1">
-            
-         {greetings.map(greetingObj =>  <EditGreeting greetingObj={greetingObj} key={greetingObj._id.toString()}/>
-
-)}      
-
-        </div>
-
-)}
-
-
+//   return (
+//     <div className="  flex flex-col px-2 gap-1">
+//       {greetings.map((greetingObj) => (
+//         <EditGreeting
+//           greetingObj={greetingObj}
+//           key={greetingObj._id.toString()}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
+}
 // <div>
 // {greetings.map((greetingObj) => <Delete greetingObj={greetingObj}  key={greetingObj._id.toString()}/>)}
 // </div>
